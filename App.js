0ded60d3689ff439ee8ef2444c80d8743755d052
Veselin Tonev <veselinn16@yahoo.com>
2019-09-04 16:30:18 +0300

    Add basic structure :construction:

--- App.js
+++ App.js
@@ -1,114 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  FlatList,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import styles from './styles/styles';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+  const [count, setCount] = useState('0');
+
+  useEffect(() => {
+    console.log(query);
+  }, [query]);
+
+  const text = useRef(null);
+
+  const changeNumber = newCount => {
+    +newCount / 2 && setCount(newCount);
+  };
+
+  const changeCount = bool => {
+    bool ? setCount(`${+count + 1}`) : setCount(`${+count - 1}`);
+  };
+
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Modify <Text style={styles.highlight}>App.js</Text> to change
-                this screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+    <View style={styles.container}>
+      <View style={styles.headingContainer}>
+        <Text style={styles.heading}>My first React-native App</Text>
+      </View>
+
+      <View style={styles.boxContainer}>
+        <View style={styles.box}>
+          <Text>1</Text>
+        </View>
+        <View style={{...styles.box, backgroundColor: '#f6ce4c'}}>
+          <Text>2</Text>
+        </View>
+        <View style={{...styles.box, backgroundColor: '#fbeab2'}}>
+          <Text ref={text}>3</Text>
+        </View>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TextInput
+          style={{
+            borderColor: 'red',
+            backgroundColor: '#fff',
+            borderRadius: 3,
+          }}
+          placeholder="Change number"
+          value={query}
+          blurOnSubmit={true}
+          onSubmitEditing={({nativeEvent}) => changeNumber(nativeEvent.text)}
+          onChangeText={query => setQuery(query)}
+        />
+        <Text>*Note: Only numbers are valud inputs</Text>
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity onPress={() => changeCount(false)}>
+            <Text style={styles.button}>Decrease Number</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => changeCount(true)}>
+            <Text style={styles.button}>Increase Number</Text>
+          </TouchableOpacity>
+        </View>
+        <Text>{count}</Text>
+      </View>
+
+      <FlatList
+        style={{flex: 5}}
+        data={[
+          {text: 'List Item 1'},
+          {text: 'List Item 2'},
+          {text: 'List Item 3'},
+          {text: 'List Item 4'},
+          {text: 'List Item 5'},
+          {text: 'List Item 6'},
+          {text: 'List Item 7'},
+          {text: 'List Item 8'},
+          {text: 'List Item 9'},
+        ]}
+        renderItem={({item, index}) => {
+          return <Text id={index}>{item.text}</Text>;
+        }}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
+      <View style={{flex: 0.5, justifyContent: 'center', alignItems: 'center'}}>
+        <Image style={{width: 50, height: 50}} source={require('./pic.png')} />
+      </View>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
