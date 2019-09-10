f348471075d97ba94f84834d4e7470b1d5db9159
Veselin Tonev <veselinn16@yahoo.com>
2019-09-10 18:00:40 +0300

    Add button component for navigation :wrench:

---
+++ components/Button.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/SimpleLineIcons';
+
+const Button = ({resource, navigation, user}) => {
+  const determineRoute = () => {
+    console.log(resource);
+    // resource.chartAt(0).toUpperCase() + resource.slice(1);
+    return 'Todos';
+  };
+
+  return (
+    <View
+      style={{
+        backgroundColor: '#555',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+      }}>
+      <TouchableOpacity
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#fff',
+          padding: 5,
+          borderRadius: 5,
+        }}
+        onPress={() => navigation.navigate(determineRoute(), {user})}>
+        <Text style={{fontSize: 17, marginRight: 7}}>{`See ${resource}`}</Text>
+        <Icon
+          name={resource === 'posts' ? 'notebook' : 'note'}
+          size={24}
+          color="black"
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Button;
