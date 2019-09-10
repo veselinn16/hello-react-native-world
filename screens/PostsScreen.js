81ebd293c34fd64900df81eead6c037c969ef06a
Veselin Tonev <veselinn16@yahoo.com>
2019-09-10 19:36:35 +0300

    Render user's posts :sparkles:

--- screens/PostsScreen.js
+++ screens/PostsScreen.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {View, FlatList} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {NavigationEvents} from 'react-navigation';
+
+const posts = require('../testPosts.json');
 
 import Heading from '../components/Heading';
+import UserPosts from '../components/UserPosts';
+import WarningMessage from '../components/WarningMessage';
 
 const PostsScreen = ({navigation}) => {
   const user = navigation.getParam('user');
+
+  const getUserPosts = userId => {
+    return posts.filter(post => post.userId === userId);
+  };
+
+  const userPosts = user ? getUserPosts(user.id) : null;
+
+  const removeCurrentUser = () => navigation.setParams({user: null});
+
   return (
-    <View>
+    <View style={{flex: 1}}>
+      <NavigationEvents onDidBlur={removeCurrentUser} />
       <Heading
         text={user ? `${user.name}'s Posts` : 'Unknown User'}
         styles={{
@@ -17,7 +32,7 @@ const PostsScreen = ({navigation}) => {
           alignItems: 'center',
         }}
       />
-      <Text>{user.name}</Text>
+      {user ? <UserPosts posts={userPosts} /> : <WarningMessage />}
     </View>
   );
 };
@@ -28,7 +43,7 @@ PostsScreen.navigationOptions = ({navigation}) => ({
     let IconComponent = Icon;
     let iconName;
     if (routeName === 'Posts') {
-      iconName = `checkbox-multiple-marked${focused ? '' : '-outline'}`;
+      iconName = `sticky-note${focused ? '' : '-o'}`;
     }
 
     return <IconComponent name={iconName} size={30} color={tintColor} />;
