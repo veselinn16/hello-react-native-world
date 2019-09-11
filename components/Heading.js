e7a8f586bbcdf18a4f6a53592430bc605db58044
Veselin Tonev <veselinn16@yahoo.com>
2019-09-11 16:50:32 +0300

    Render children when available :wrench:

--- components/Heading.js
+++ components/Heading.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-const Heading = ({text, styles}) => (
+const Heading = ({text, styles, children}) => (
   <View style={{padding: 10, ...styles}}>
     <Text style={{color: '#fff', textAlign: 'center', fontSize: 20}}>
       {text}
     </Text>
+    {children}
   </View>
 );
 
