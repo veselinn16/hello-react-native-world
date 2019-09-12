16f072c456956fb3e6eca493d8e66878ff821d70
Veselin Tonev <veselinn16@yahoo.com>
2019-09-12 17:51:44 +0300

    Update component to handle data fetched from API :wrench:

--- components/UsersList.js
+++ components/UsersList.js
@@ -1,10 +1,10 @@
 import React, {Fragment} from 'react';
-import {View, FlatList, Text} from 'react-native';
+import {FlatList} from 'react-native';
 
 import UserButton from './UserButton';
 import Heading from './Heading';
 
-const UsersList = ({users, data, navigation}) => {
+const UsersList = ({users, navigation}) => {
   return (
     <Fragment>
       <Heading text={'Users'} styles={{backgroundColor: '#555'}} />
@@ -12,7 +12,7 @@ const UsersList = ({users, data, navigation}) => {
         data={users}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({item}) => {
-          const evenIndex = data.indexOf(item) % 2 == 0 ? true : false;
+          const evenIndex = users.indexOf(item) % 2 == 0 ? true : false;
           return (
             <UserButton
               user={item}
