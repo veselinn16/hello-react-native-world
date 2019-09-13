8b33ab2600b8c707ab55ddb0c1197b64579c190f
Veselin Tonev <veselinn16@yahoo.com>
2019-09-13 17:10:02 +0300

    Add redux-thunk middleware :wrench:

--- store/index.js
+++ store/index.js
@@ -1,4 +1,9 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reducers from '../reducers';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
-export default createStore(reducers);
+export default createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
